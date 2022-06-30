629499ddc843e9a1b2bf80c7b0adfd6d6217f667
Caio Lourenço <lourencocaio228@gmail.com>
2022-06-30 07:57:02 -0300

    Topicos especiais Csharp

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Extensios;
+
+namespace Extensios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2022, 06, 29, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
